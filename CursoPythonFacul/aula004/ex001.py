1f225a2f2e709ab4d917b9a2a7f4f10274f92099
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-05-13 19:20:44 -0300

    Update ex001.py

--- CursoPythonFacul/aula004/ex001.py
+++ CursoPythonFacul/aula004/ex001.py
@@ -12,7 +12,7 @@ nasc = int(input("Qual o ano de nascimento: "))
 idade = date.today().year - nasc
 
 print(f"Olá, seu nome é {nome} e tem {idade} anos!")
-
+'''
 soma = num = media = valor = 0
 nota = 1
 for c in range(0, 3):
@@ -27,7 +27,7 @@ for c in range(0, 3):
         media = 0
         break
 print(f"A media do aluno é de {media}")
-
+'''
 oposto = float(input("Digite o valor do cateto oposto: "))
 adjascente = float(input("Digite o valor do cateto adjascente: "))
 hipotenusa = float(input("Digite o valor da hipotenusa: "))
@@ -45,8 +45,14 @@ print(f"A tag dos angulos é {tg}")
 n1 = int(input("Digite um numero: "))
 pot = n1 ** 2
 print(f"O valor da conta é {pot % 3}")
-'''
+
 
 frase = str(input("Digite uma frase: "))
 print(len(frase))
+
+print(frase.replace("pyhon"))
+print(frase[4])
+print(frase.upper())
+print(frase.lower())
+'''
 
