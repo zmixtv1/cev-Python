b88711b931be78372b540f3059ddbbb05707d7ef
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-05-18 16:17:33 -0300

    Update aula016.py

---
+++ python-teste/aula016.py
@@ -0,0 +1,17 @@
+'''lanche = ("hamburguer", "suco", "pizza", "pudim", "batata frita")
+for cont in range(0, len(lanche)):
+    print(f"eu vou comer {lanche[cont]}!")
+
+for comida in lanche:
+    print(f"eu vou conmer {comida}!")
+'''
+'''
+a = (2, 5 ,4)
+b = (5, 8, 1, 2)
+c = a + b
+print(c)
+print(c.index(2, 1))
+'''
+pessoa = ("Gustavo", 39, "M", 99.88)
+del pessoa
+print(pessoa)
