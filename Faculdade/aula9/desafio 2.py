1012e0268ad9f4d2a5a9e7558b316f5467a80223
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-05-20 20:52:21 -0300

    Update desafio 2.py

--- Faculdade/aula9/desafio 2.py
+++ Faculdade/aula9/desafio 2.py
@@ -1,19 +1,19 @@
 from graphics import *
-from graphics import *
 from time import sleep
 import random
 janela = GraphWin("Quadrados ...", 600, 600)
 janela.setBackground("gray")
 tam = 3
-for c in range(66):
+for c in range(67):
     circulo = Circle(Point(300, 300), tam)
     cor = color_rgb(random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
     circulo.setFill(cor)
     circulo.setOutline("black")
     circulo.draw(janela)
-    sleep(0.1)
     tam += 3
-    if tam == 198:
+    print(c)
+    sleep(0.1)
+    if tam == 199:
         break
 
 janela.getMouse()
