aa70f48e804ee7fb94459d2ff88e44a1e50b9526
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-05-26 18:28:55 -0300

    renomear

---
+++ python-teste/aula017a.py
@@ -0,0 +1,14 @@
+#adicionar elemento na lista
+'lista.append(objeto)'
+#adicionar em qualquer posição
+"lista.insert(local, objeto)"
+#apagar algo da lista!
+"del lista[3]"
+#apagar ultimo item da lista
+"lista.pop()"
+#apagar um objeto que vc quer!
+"lista.remove('objeto')"
+#colocar itens em ordem
+"lista.sort()"
+#colocar itens em ordem inversa!
+"lista.sort(reverse=TRUE)"
