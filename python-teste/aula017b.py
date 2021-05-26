0c5902eb323e30d4e4c51b4245ab0b2c2cec6585
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-05-26 19:03:04 -0300

    Update aula017b.py

---
+++ python-teste/aula017b.py
@@ -0,0 +1,20 @@
+'''teste = list()
+teste.append("Gustavo")
+teste.append(40)
+galera = list()
+galera.append(teste[:])
+teste[0] = "maria"
+teste[1] = 22
+galera.append(teste[:])
+print(galera)
+print(galera[0][0],galera[1][1])'''
+
+galera = list()
+dados = list()
+for c in range(0, 3):
+    dados.append(str(input("Nome: ")))
+    dados.append(int(input("Idade: ")))
+    galera.append(dados[:])
+    dados.clear()
+    
+print(galera)
