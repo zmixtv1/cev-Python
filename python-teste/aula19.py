92107faf35c208ce2ebe6fd3ec658497675e852c
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-06-02 16:52:04 -0300

    a

--- python-teste/aula19.py
+++ python-teste/aula19.py
@@ -1,3 +1,18 @@
-pessoas = {"nome": "Gustavo", "Sexo": "M", "Idade": 22}
+'''pessoas = {"nome": "Gustavo", "Sexo": "M", "Idade": 22}
 for k, v in pessoas.items():
     print(f"{k} = {v}")
+'''
+estado = dict()
+brasil = list()
+for c in range(0,3):
+    estado["uf"] = str(input("Unidade federativa: ")).capitalize()
+    estado["Sigla"] = str(input("Sigla do Estado: ")).capitalize()
+    brasil.append(estado.copy())
+for e in brasil:
+    '''
+    for k, v in e.items():
+        print(f"O campo {k} tem valor {v}.")
+    '''
+    for v in e.values():
+        print(v, end=" ")
+    print("fim")
