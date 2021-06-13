bc079c284fedb47441b115db2caf5375567bafb0
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-06-13 12:59:37 -0300

    Update aula021.py

--- python-teste/aula021.py
+++ python-teste/aula021.py
@@ -18,7 +18,7 @@ contador(0, 100, 10)
 help(contador)
 '''
 
-
+'''
 def somar(a=0, b=0, c=0):
     s = a + b + c
     print(f"A soma vale {s}")
@@ -27,3 +27,30 @@ def somar(a=0, b=0, c=0):
 somar(3, 2, 5)
 somar(8, 4)
 somar()
+'''
+
+'''
+def teste(b):
+    global a
+    a = 8
+    b += 4
+    c = 2
+    print(f"A dentro vale {a}")
+    print(f"B dentro vale {b}")
+    print(f"C dentro vale {c}")
+
+
+a= 5
+teste(a)
+print(f"A fora vale {a}")
+'''
+
+def somar(a=0, b=0, c=0):
+    s = a + b + c
+    return s
+
+
+r1 = somar(3, 2, 5)
+r2 = somar(1, 7)
+r3 = somar(4)
+print(f"Meus cáuculos deram {r1}, {r2}, {r3}")
