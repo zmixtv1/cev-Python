cccaf32bd636ed3e8a81cd2a45238188ed511b47
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-06-13 12:13:37 -0300

    Update arquivo.py

--- Faculdade/aula11/arquivo.py
+++ Faculdade/aula11/arquivo.py
@@ -1,6 +1,6 @@
 def tabuadas():
     num = int(input('Digite o número para a tabuada: '))
-    arq_tabuada = open('c:\\temp\\tabuada.txt', 'w')
+    arq_tabuada = open('f:\\z.doc\\tabuada.txt', 'w')
     for i in range(1, 11):
         arq_tabuada.write(f'{num} x {i} = {num * i} \n')
     arq_tabuada.write('\r')
@@ -8,16 +8,13 @@ def tabuadas():
 
 
 def imprimir():
-    arq_tabuada = open('c:\\temp\\tabuada.txt', 'r')
-    print("-=" * 10)
-    print(f'{"Tabuada" :^21}')
+    arq_tabuada = open('f:\\z.doc\\tabuada.txt', 'r')
     for c in arq_tabuada:
         print(f"{c}", end="")
-    print("-=" * 10)
     arq_tabuada.close()
 
 
 tabuadas()
-imprimir()
 
+imprimir()
 
