bdb7db92f6e74f0d8189ab87da7364633d2649a2
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-06-13 15:17:49 -0300

    hg

---
+++ PycharmProjects/pythonexercicios/aula21/ex103.py
@@ -0,0 +1,28 @@
+'''
+def ficha(nome, gols):
+    if nome == "":
+        nome = "<desconhecido>"
+    if gols == "":
+        gols = 0
+    print(f"o jogador {nome} fez {gols} gols no campeonato")
+
+
+nome = str(input("Nome do Jogador: "))
+gols = input("Quantos gols ele marcou: ")
+ficha(nome, gols)
+'''
+
+
+def ficha(jog="<desconhecido>", gols=0)
+    print(f"o jogador {jog} fez {gols} gol(s) no campeonato.")
+
+n = str(input("Nome do jogador: "))
+g= int(input("número de Gols: "))
+if g.isnumeric():
+    g = int(g)
+else:
+    g = 0
+if n.strip() == "":
+    ficha(gol=g)
+else:
+    ficha(n,g)
