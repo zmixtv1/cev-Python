2b917b9f4f178344653c39fd54ab8fedaa8c833b
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-06-19 18:42:06 -0300

    a

--- PycharmProjects/pythonexercicios/aula022/moeda/__init__.py
+++ PycharmProjects/pythonexercicios/aula022/moeda/__init__.py
@@ -1,18 +1,34 @@
-def aumentar(preço,taxa):
+def aumentar(preço=0,taxa=0,formato=False):
     res = preço + (preço* taxa/100)
-    return res
+    return res if formato is False else moeda(res)
 
 
-def diminuir(preço,taxa):
+def diminuir(preço=0,taxa=0, formato=False):
     res = preço - (preço * taxa/100)
-    return res
+    return res if formato is False else moeda(res)
 
 
-def dobro(preço):
+def dobro(preço=0, formato=False):
     res = preço * 2
-    return res
+    return res if formato is False else moeda(res)
 
 
-def metade(preço):
+def metade(preço=0, formato=False):
     res = preço / 2
-    return res
+    return res if formato is False else moeda(res)
+
+
+def moeda(preço=0, moeda="R$"):
+    return f"{moeda}{preço:.2f}".replace(".",",")
+
+
+def resumo(preço=0, taxaa=10, taxar=5):
+    print("~"*30)
+    print("Resumo do valor".center(30))
+    print("~"*30)
+    print(f"Preço analisado:\t{moeda(preço)}")
+    print(f"Dobro do preço: \t{dobro(preço,True)}")
+    print(f"Metade do preço:\t{metade(preço,True)}")
+    print(f"Com {taxaa}% de aumento:\t{aumentar(preço,taxaa,True)}")
+    print(f"Com {taxar}% de redução:\t{diminuir(preço,taxar,True)}")
+    print("~"*30)
