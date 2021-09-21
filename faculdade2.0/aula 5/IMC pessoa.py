5ece502d98adab1f90ae7448545f55b09c8e6b54
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-09-21 20:23:11 -0300

    Update IMC pessoa.py

--- faculdade2.0/aula 5/IMC pessoa.py
+++ faculdade2.0/aula 5/IMC pessoa.py
@@ -9,17 +9,26 @@ class pessoa(object):
 
 
     def get_nome(self):
-        return  self.nome
+        return self.nome
     def set_nome(self,novo_nome):
         self.nome = novo_nome
     def get_data_de_nascimento(self):
-        return  self.data_de_nascimento
+        return self.data_de_nascimento
     def set_data_de_nascimeto(self,nova_data):
         self.data_de_nascimento = nova_data
+    def imc(self):
+        vl_imc = self.peso/(self.altura ** 2)
+        return vl_imc
 
 
 
 if __name__ == "__main__":
     data_de_nascimento = datetime.date(1993,12,13)
-    pessoa1 = pessoa("Rodrigo", 71, 1.80, data_de_nascimento)
+    pessoa1 = pessoa("Rodrigo", 50, 1.83, data_de_nascimento)
     print(pessoa1)
+
+    nome = pessoa1.get_nome()
+    print(f"Pessoa 1 = {nome}")
+    pessoa1.set_nome("Jorge")
+
+    
