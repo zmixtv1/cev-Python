97d2de17682e334e26248fb5af389e677b9d2cf7
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-10-25 19:25:01 -0300

    Create Rodrigo_alaor.py

---
+++ faculdade2.0/aula 8/Rodrigo_alaor.py
@@ -0,0 +1,9 @@
+
+
+class dicionario(object):
+    def __init__(self,classe_gramatical,regencia,divisao_silabica):
+        self.classe_gramatical = classe_gramatical
+        self.regencia = regencia
+        self.divisao_silabica = divisao_silabica
+
+    def
