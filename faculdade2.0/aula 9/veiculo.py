a49daaded733f2f5570c71352451a5a6484a9d54
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-10-26 19:55:00 -0300

    Update veiculo.py

--- faculdade2.0/aula 9/veiculo.py
+++ faculdade2.0/aula 9/veiculo.py
@@ -21,8 +21,53 @@ class carro(veiculo):
         super().__init__(valor,modelo,km)
         self.qtd_portas = qtd_portas
 
+    def get_qtd_portas(self):
+        return self.qtd_portas
+
+
+    def __str__(self):
+        s = f"valor: {self.valor}, Modelo: {self.modelo}, KM: {self.km}, Qtd.portas: {self.qtd_portas}"
+        return s
+
+class moto(veiculo):
+    def __init__(self, valor, modelo, km=1, cilindradas=2):
+        super().__init__(valor, modelo, km)
+        self.cilindradas = cilindradas
+
+    def get_cilindradas(self):
+        return self.cilindradas
+    def set_silindradas(self,n_cilindrada):
+        self.cilindradas = n_cilindrada
+
+    def __str__(self):
+        s = f"valor: {self.valor}, Modelo: {self.modelo}, KM: {self.km}, cilindradas: {self.cilindradas}"
+        return s
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
-     
+    c1 = carro(990000, "Civic", 1000, 4)
+    print(c1)
+    print(f"Modelo: {c1.get_modelo()}")
+    c2 = carro(100000, "corola", 0)
+    print(c2)
+    print(f"Modelo: {c2.get_modelo()}")
+    c3 = carro(70000,"HB20")
+    print(c3)
+    print(f"Modelo: {c3.get_modelo()}")
+
+    m1 = moto(30000, "BMW", 5000, 10)
+    print(m1)
+    m2 = moto(32000, "cbs", 12000, 400)
+    print(m2)
