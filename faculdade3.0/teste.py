edce04f86d943d9af070bf3c5e89575ff796ec9e
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2021-12-24 22:55:06 -0300

    Update teste.py

--- faculdade3.0/teste.py
+++ faculdade3.0/teste.py
@@ -19,5 +19,22 @@ class carro(veiculo):
     def __init__(self, placa, preco, qtd_portas):
         self.qtd_portas = qtd_portas
         super().__init__(placa,preco)
+    def set_qtd_portas(self,n_qtd):
+        if n_qtd < 2 and n_qtd > 4:
+            print("Quantidade de portas invalida.")
+        else:
+            self.qtd_portas = n_qtd
 
-    
+        return self.qtd_portas
+
+
+    def calcula_ipva(self):
+        ipva = self.preco * 0.03
+        print(f"O valor do IPVA é {ipva}")
+
+class onibus(veiculo):
+    def __init__(self,placa, preco, capacidade):
+        self.capacidade = capacidade
+        super().__init__(placa,preco)
+
+    def get_capacidade
